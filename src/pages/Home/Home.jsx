7c0f5fce645ea051dda15b92c268cb2cae42ea90
Abhi solanki <abhi85076@gmail.com>
2026-03-25 17:23:14 +0530

    just fix container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -154,7 +154,7 @@ const Home = () => {
     <>
       <Navbar userInfo={userInfo} onSearchNote={onSearchNote} handleClearSearch={handleClearSearch} />
 
-      <div className='container mx-auto'>
+      <div className='max-w-7xl mx-auto'>
         { allNotes.length > 0 ? <div className='grid grid-cols-3 gap-4 mt-8'>
           {allNotes.map((item, index) => (
             <NoteCard
